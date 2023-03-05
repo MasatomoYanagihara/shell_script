9a225f97e61b96a593091b7e0b9cb5981e463659
Yana <k87pro@gmail.com>
2023-03-05 21:03:58 +0900

    Fix: コメントアウト取り除く

--- for_loop.sh
+++ for_loop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 基本形
-# for f in file1 file2 file3 file4 file5; do
-#   cp -p "${f}" "${f}.$(date +%Y%m%d)"
-# done
+for f in file1 file2 file3 file4 file5; do
+  cp -p "${f}" "${f}.$(date +%Y%m%d)"
+done
 
 
 # 変数にまとめるバージョン
-# files='file1 file2 file3 file4 file5'
-# for f in ${files}; do # ここのfiles変数はダブルコーテーションで囲まない
-#   cp -p "${f}" "${f}.$(date +%Y%m%d)"
-# done
+files='file1 file2 file3 file4 file5'
+for f in ${files}; do # ここのfiles変数はダブルコーテーションで囲まない
+  cp -p "${f}" "${f}.$(date +%Y%m%d)"
+done
 
 
 # 変数にまとめるバージョン（改行）
@@ -29,6 +29,7 @@ done
 for f in file*; do
   cp -p "${f}" "${f}.$(date +%Y%m%d)"
 done
+
 
 # コマンド置換を使用するバージョン
 for f in $(cd /home/testdir || exit; ls | grep -v tmpfile); do
